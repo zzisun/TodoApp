2ffe5613addc52e63a34d564d6c9ea65e4e118fa
jeong-inho <derosatam76@gmail.com>
2021-04-16 15:02:45 +0900

    Feat : 목업 데이터 수정
    대충 기록했던 목업 데이터를 실제 서비스에 맞게 수정

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 insert into user (user_id, password, name, email) values ('user1', '1234', '이노', 'eno@gmail.com');
 insert into user (user_id, password, name, email) values ('user2', '1234', '트리', 'tree@gmail.com');
 
-insert into work (title, content, created_at, updated_at, status, author_id) values('no1', 'create first todo', '2021-04-08 16:40', '2021-04-08 16:40', 'TODO', 1);
-insert into work (title, content, created_at, updated_at, status, author_id) values('no2', 'create first doing', '2021-04-08 16:45', '2021-04-08 16:45', 'DOING', 1);
-insert into work (title, content, created_at, updated_at, status, author_id) values('no3', 'create second todo', '2021-04-08 16:50', '2021-04-08 16:50', 'TODO', 1);
-insert into work (title, content, created_at, updated_at, status, author_id) values('no4', 'create second doing', '2021-04-08 16:55', '2021-04-08 16:55', 'DOING', 1);
-insert into work (title, content, created_at, updated_at, status, author_id) values('no5', 'create first done', '2021-04-08 17:00', '2021-04-08 17:00', 'DONE', 1);
+insert into work (title, content, created_at, updated_at, status, author_id) values('GitHub 공부하기', 'add, commit, push', '2021-04-08 16:40', '2021-04-08 16:40', 'TODO', 1);
+insert into work (title, content, created_at, updated_at, status, author_id) values('블로그에 포스팅할 것', '* GitHub 공부내용', '2021-04-08 16:45', '2021-04-08 16:45', 'TODO', 1);
+insert into work (title, content, created_at, updated_at, status, author_id) values('HTML/CSS 공부하기', 'input 태그 실습', '2021-04-08 16:50', '2021-04-08 16:50', 'DOING', 1);
+insert into work (title, content, created_at, updated_at, status, author_id) values('Java 공부하기', 'Java의 정석 챕터10 읽기', '2021-04-08 16:55', '2021-04-08 16:55', 'DOING', 1);
+insert into work (title, content, created_at, updated_at, status, author_id) values('Spring boot 공부하기', 'intercepter 적용하기', '2021-04-08 17:00', '2021-04-08 17:00', 'DONE', 1);
 
-insert into timeline (content, created_at, author_id) values('해야할 일에 no1를 등록하였습니다.', '2021-04-08 16:40', 1);
-insert into timeline (content, created_at, author_id) values('하고 있는 일에 no2를 등록하였습니다.', '2021-04-08 16:40', 1);
-insert into timeline (content, created_at, author_id) values('해야할 일에 no3를 등록하였습니다.', '2021-04-08 16:40', 1);
-insert into timeline (content, created_at, author_id) values('하고 있는 일에 no4를 등록하였습니다.', '2021-04-08 16:40', 1);
-insert into timeline (content, created_at, author_id) values('완료한 일에 no5를 등록하였습니다.', '2021-04-08 16:40', 1);
+insert into timeline (content, created_at, author_id) values('해야할 일에 "GitHub 공부하기"를 등록하였습니다.', '2021-04-08 16:40', 1);
+insert into timeline (content, created_at, author_id) values('해야할 일에 "블로그에 포스팅할 것"을 등록하였습니다.', '2021-04-08 16:40', 1);
+insert into timeline (content, created_at, author_id) values('하고 있는 일에 "HTML/CSS 공부하기"를 등록하였습니다.', '2021-04-08 16:40', 1);
+insert into timeline (content, created_at, author_id) values('하고 있는 일에 "Java 공부하기"를 등록하였습니다.', '2021-04-08 16:40', 1);
+insert into timeline (content, created_at, author_id) values('완료한 일에 "Spring boot 공부하기"를 등록하였습니다.', '2021-04-08 16:40', 1);
